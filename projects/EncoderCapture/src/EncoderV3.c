bbc12ec8cb23de6ac37c118bdb7a4aea2e881cfe
Javier Kipen <javier1kipen@gmail.com>
2020-10-08 11:59:11 -0300

    Encoder con DMA no va
    No se puede configurar al periférico timer para que funcione con dma cada vez que captura. Quizás con otro periférico va

--- projects/EncoderCapture/src/EncoderV3.c
+++ projects/EncoderCapture/src/EncoderV3.c
@@ -25,14 +25,12 @@ typedef struct {
 	unsigned int measArray[MAX_ENC_SAMPLES];
 	unsigned int measCount;
 	IRQn_Type  timerIrqAddr;
+	uint8_t dmaChannelNum;
 }enc_config_t;
 /*==================[internal data declaration]==============================*/
-static enc_config_t encR = { 6, 1, 5, LPC_TIMER2, 2, 0, 2, 8, {},0,TIMER2_IRQn};
-static enc_config_t encL = { 2, 5, 1, LPC_TIMER0, 0, 2, 0, 2, {},0,TIMER0_IRQn};
+static enc_config_t encR = { 6, 1, 5, LPC_TIMER2, 2, 0, 2, 8, {},0,0};
+static enc_config_t encL = { 2, 5, 1, LPC_TIMER0, 0, 2, 0, 2, {},0,0};
 enc_config_t * getEncoderPointer(ENCODER_CHANNEL_T ch);
-void TIMER0_IRQHandler(void);
-void TIMER2_IRQHandler(void);
-void interruptRoutine(ENCODER_CHANNEL_T ch);
 
 /*==================[internal functions definition]==========================*/
 enc_config_t * getEncoderPointer(ENCODER_CHANNEL_T ch)
@@ -60,13 +58,13 @@ void EncoderV2_Init(ENCODER_CHANNEL_T ch)
 	Chip_TIMER_Init(encoder->timer);
 	Chip_TIMER_PrescaleSet(encoder->timer, 1);	// Timer res = 204MHz/100 = 2.04MHz
 	Chip_TIMER_CaptureRisingEdgeEnable(encoder->timer,encoder->captureNum); //Captura los datos en flancos ascendentes
-	Chip_TIMER_CaptureEnableInt(encoder->timer,encoder->captureNum);//Habilito un interrupt cada vez que ocurre ese flanco ascendente
 
+	Chip_GPDMA_Init(LPC_GPDMA);
 
-
-	NVIC_SetPriority(encoder->timerIrqAddr, 5 ); // FreeRTOS Requiere prioridad >= 5 (numero mas alto, mas baja prioridad)
-	// Enable Interrupt for TIMERX channel
-	NVIC_EnableIRQ( encoder->timerIrqAddr);
+	encoder->dmaChannelNum = Chip_GPDMA_GetFreeChannel(LPC_GPDMA, GPDMA_CONN_UART0_Rx );
+	NVIC_DisableIRQ(DMA_IRQn);
+	NVIC_SetPriority(DMA_IRQn, 5 ); // FreeRTOS Requiere prioridad >= 5 (numero mas alto, mas baja prioridad)
+	NVIC_EnableIRQ(DMA_IRQn);
 
 	Chip_TIMER_Enable(encoder->timer);
 }
@@ -87,7 +85,6 @@ uint32_t EncoderV3_GetCountFiltered(ENCODER_CHANNEL_T ch,uint32_t minCount,uint3
 {
 	uint32_t measAverage=0,totalCount=0;
 	enc_config_t * encoder=getEncoderPointer(ch);
-	NVIC_DisableIRQ( encoder->timerIrqAddr); //Deshabilito interrupciones
 	for(unsigned int i=0;i<encoder->measCount;i++)
 	{
 		uint32_t meas = encoder->measArray[i];
@@ -97,32 +94,10 @@ uint32_t EncoderV3_GetCountFiltered(ENCODER_CHANNEL_T ch,uint32_t minCount,uint3
 	}
 	if(totalCount>0)
 		measAverage /= totalCount;
-	NVIC_EnableIRQ( encoder->timerIrqAddr); //Habilita interrupción de vuelta
 	return measAverage;
 }
 
 void EncoderV3_ResetCount(ENCODER_CHANNEL_T ch)
 {
 	Chip_TIMER_Reset( ch == ENCODER_RIGHT ? encR.timer : encL.timer);
-}
-
-/*==================[Interrupt]=========================*/
-void TIMER0_IRQHandler(void) //Ambas rutinas de interrupt llaman a una función en común
-{
-	interruptRoutine(ENCODER_LEFT);
-	//Chip_TIMER_Reset(encL.timer); //Esto debería ir en el caso en el que no se clearee solo el capture reg (no creo que pase)
-}
-void TIMER2_IRQHandler(void)
-{
-	interruptRoutine(ENCODER_RIGHT);
-	//Chip_TIMER_Reset(encR.timer); //Esto debería ir en el caso en el que no se clearee solo el capture reg (no creo que pase)
-}
-
-void interruptRoutine(ENCODER_CHANNEL_T ch)
-{
-	enc_config_t * encoder=getEncoderPointer(ch);
-	if(encoder->measCount<MAX_ENC_SAMPLES) //En el caso que no llené el buffer con mediciones ya
-		encoder->measArray[(encoder->measCount)++]=Chip_TIMER_ReadCount(encoder->timer); //Pongo la medición obtenida
-	Chip_TIMER_ClearCapture(encoder->timer, encoder->captureNum);
-	//Acá se podría poner disable del interrupt en caso de solo tomar una medición en vez de un promedio
 }
